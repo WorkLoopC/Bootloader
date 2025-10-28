0392913a8f7c1a1266243e25c44d59bba41f8115
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2025-10-28 12:42:25 +0100

    bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,11 +1,25 @@
-[BITS 16]
-[ORG 0x1000]
+BITS 16               
+ORG 0x7C00         
+start:
 
-_start:
-    mov ah, 0x0E    
-    mov al, 'H'
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00     
+    mov si, message
+print_char:
+    lodsb          
+    or  al, al        
+    jz  done_print
+    mov ah, 0x0E       
     int 0x10
-    mov al, 'i'
-    int 0x10
-hang:
-    jmp hang
+    jmp print_char
+
+done_print:
+    cli          
+    hlt              
+
+
+times 510-($-$$) db 0
+dw 0xAA55
